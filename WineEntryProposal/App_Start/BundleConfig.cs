a9f0edc01c2176dc0e50e958eef46f92c0594c8f
jfouts1979 <Fouts.John@gmail.com>
2017-03-24 13:44:41 -0400

    Updated Scripts.

--- WineEntryProposal/App_Start/BundleConfig.cs
+++ WineEntryProposal/App_Start/BundleConfig.cs
@@ -19,6 +19,8 @@ namespace WineEntryProposal
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/slider").Include("~/Scripts/jquery.bxslider.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
