ebdfd4b4ee77351adbadebde4c3d254115148396
jfouts1979 <Fouts.John@gmail.com>
2017-03-24 13:38:16 -0400

    Adding Slider jQuery Control to Customize Appearnce of HomePage with Sliding Feature.  bxSlider is the jQuery Slider I am using.  Added New Slider Controler.  Added New OurDBContext file that I will rename later.  This is to store media for the website so for all purposes belongs in a separate database from the winery inventory information.

---
+++ WineEntryProposal/Controllers/SliderController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WineEntryProposal.Models;
+
+namespace WineEntryProposal.Controllers
+{
+    public class SliderController : Controller
+    {
+        // GET: Slider
+        public ActionResult Index()
+        {
+
+            using (OurDbContext db = new OurDbContext())
+            {
+
+                return View(db.gallery.ToList());
+
+            }
+            // return View();
+        }
+
+        // Add images in slider
+
+        public ActionResult AddImage()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult AddImage(HttpPostedFileBase ImagePath)
+        {
+            if (ImagePath != null)
+            {
+                // This forces user to upload images of 
+                // specified resolution.
+                System.Drawing.Image img = System.Drawing.Image.FromStream(ImagePath.InputStream);
+                if ((img.Width != 800 || img.Height != 356))
+                {
+                    ModelState.AddModelError("", "Image resolution must be 800 x 356 pixels.");
+                    return View();
+                }
+                // Upload pic
+                string pic = System.IO.Path.GetFileName(ImagePath.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/Content/Images/"), pic);
+                ImagePath.SaveAs(path);
+                using (OurDbContext db = new OurDbContext())
+                {
+                    Gallery gallery = new Gallery { ImagePath = "~/Content/Images/" + pic };
+                    db.gallery.Add(gallery);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Delete multiple copies of images
+
+        public ActionResult DeleteImages()
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                return View(db.gallery.ToList());
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult DeleteImages(IEnumerable<int> ImagesIDs)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                foreach (var id in ImagesIDs)
+                {
+                    var image = db.gallery.Single(s => s.ID == id);
+                    string imgPath = Server.MapPath(image.ImagePath);
+                    db.gallery.Remove(image);
+                    if (System.IO.File.Exists(imgPath))
+                        System.IO.File.Delete(imgPath);
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("DeleteImages");
+        }
+
+    }
+}
---
+++ WineEntryProposal/Models/OurDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WineEntryProposal.Models
+{
+    public class OurDbContext : DbContext
+    {
+    public DbSet<Gallery> gallery { get; set; }
+    }
+}
