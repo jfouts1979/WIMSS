f4b9033e80d0397c64b4e1c8ed6c96a767ec1d27
jfouts1979 <Fouts.John@gmail.com>
2017-03-29 22:58:23 -0400

    Checked In 6 image files.  Hopefully people can see those files now on load of slider.  Modified MediaContext.cs to include skeleton of seed method.

--- WineEntryProposal/Models/MediaContext.cs
+++ WineEntryProposal/Models/MediaContext.cs
@@ -6,8 +6,43 @@ using System.Web;
 
 namespace WineEntryProposal.Models
 {
+    // ********************************************
+    // *** Drop and Create the Database Only ******
+    // *** If the Model Changes *******************
+    // ********************************************
+
+    public class MediaDatabaseInitializer : DropCreateDatabaseIfModelChanges<MediaContext>
+    {
+
+        // **********************************************
+        // *** Seed the Database With the Data Below ****
+        // **********************************************
+
+        protected override void Seed(MediaContext context)
+        {
+
+            // *******************************************
+            // *** Images To Populate Slider *************
+            // *******************************************
+
+            Gallery gallery = new Gallery
+            {
+               //need to add code here to populate slider...
+            };
+
+
+        }
+        
+        //*** this code is not correct ***
+        //*** need to correct this code possibly to seed database with images
+        //*** may not be necessary *** - should not be.
+        //context.gallery.Add(gallery);
+        //    context.SaveChanges();
+        //    base.Seed(context);
+    }
+
     public class MediaContext : DbContext
     {
-    public DbSet<Gallery> gallery { get; set; }
+        public DbSet<Gallery> gallery { get; set; }
     }
 }
