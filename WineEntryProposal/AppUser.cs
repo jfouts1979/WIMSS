46f06edd7595f0ea1390f06a95b0a4d9019d6c00
John R. Fouts, MBA <Fouts.John@gmail.com>
2017-11-11 23:13:22 -0500

    Added AppUser Class

---
+++ WineEntryProposal/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WineEntryProposal
+{
+    public class AppUser : IdentityUser
+    {
+        //add your custom properties which have not included in IdentityUser before
+        public string MyExtraProperty { get; set; }
+    }
+}
